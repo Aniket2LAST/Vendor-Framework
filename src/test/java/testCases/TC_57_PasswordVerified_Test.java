fefc2af540de4d85901afc8554504d3977fe1d3c
Aniket <aniket@fleekitsolutions.com>
2025-06-30 11:26:38 +0530

    fourth commit

--- src/test/java/testCases/TC_57_PasswordVerified_Test.java
+++ src/test/java/testCases/TC_57_PasswordVerified_Test.java
@@ -7,7 +7,7 @@ import testBase.BaseClass;
 
 public class TC_57_PasswordVerified_Test extends BaseClass {
     
-	@Test()
+	@Test
 	public void isPassword() throws InterruptedException {
 		Password_Verification_Page np=new Password_Verification_Page(driver);
 		np.isPasswordChange();
